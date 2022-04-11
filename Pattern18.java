5a02c27a292d07c10c29d597eda3dd8f706c47a6
princekumarb <97432237+princekumarb@users.noreply.github.com>
2022-04-11 10:37:34 +0530

    Create Pattern18.java

---
+++ Pattern18.java
@@ -0,0 +1,31 @@
+package Pattern;
+
+public class Pattern18 {
+    public static void main(String args[])
+    {
+        printPatten();
+    }
+
+    private static void printPatten() {
+        int i,j,k=0;
+        for(i=1;i<=9;i++)
+        {
+            if(i<=5)
+                k++;
+            else
+                k--;
+            for(j=1;j<=9;j++)
+            {
+                if(j<=6-k || j>=4+k)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}

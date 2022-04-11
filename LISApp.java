1d65f893f028be27f758a745b7633cf2c4f5157c
princekumarb <97432237+princekumarb@users.noreply.github.com>
2022-04-11 10:30:20 +0530

    Create LISApp.java

---
+++ LISApp.java
@@ -0,0 +1,36 @@
+package DynamicPrograming;
+
+//Get Longest Increasing Subsequence Length
+public class LISApp {
+
+    public static int lisLength(int[] a) {
+        int[] lis = new int[a.length];
+
+        for (int i = 0; i < lis.length; i++) {
+            lis[i] = 1;
+        }
+
+        int maxLisLength = 1;
+
+        for (int i = 1; i < lis.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (a[i] > a[j] && 1 + lis[j] > lis[i]) {
+                    lis[i] = lis[j] + 1;
+
+                    maxLisLength = Math.max(maxLisLength, lis[i]);
+                }
+            }
+        }
+
+        return maxLisLength;
+    }
+
+    public static void main(String[] args) {
+
+        int a[] = { 7, 1, 4, 8, 11, 2, 14, 3 };
+
+        System.out.println(LISApp.lisLength(a));
+
+    }
+
+}
